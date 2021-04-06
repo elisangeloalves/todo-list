1a27b946f9d8b517c19f374c8ddcc976b07b0cbf
Preto115 <preto115@gmail.com>
2021-04-05 22:14:27 -0300

    adicionando a pasta target do projeto e retirando do .gitignore

---
+++ target/classes/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO task(task_name, status) VALUES('Investir em cursos e certificacoes', 'pending');
+INSERT INTO task(task_name, status) VALUES('Iniciar transição de carreira', 'completed');
+INSERT INTO task(task_name, status) VALUES('Fazer projeto para o avaliação técnica', 'completed');
+INSERT INTO task(task_name, status) VALUES('Pagar as contas atrasadas', 'pending');
